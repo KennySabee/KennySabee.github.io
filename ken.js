a12d9c8fdd113c114470c3d799bd5cc210ef1d00
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2022-11-05 17:15:19 -0500

    feat:change dbjson

--- ken.js
+++ ken.js
@@ -2,25 +2,24 @@ let personajesLabel = [],
   especiesLabel = [],
   generoLabel = [];
 
-
-
 // GRAFICO DE CHART JS
 async function personajesChart() {
   await getApiData();
 
   const grafico = document.getElementById("myChart").getContext("2d");
   const myChart = new Chart(grafico, {
-    type: 'pie',
+    type: "pie",
     data: {
-        datasets: [{
-        data: {
-            personajesLabel
-        }
-        }]
-    }
-    })}
-    
-    
+      datasets: [
+        {
+          data: {
+            personajesLabel,
+          },
+        },
+      ],
+    },
+  });
+}
 
 personajesChart();
 //     type: "bar",
@@ -32,14 +31,14 @@ personajesChart();
 //           data: especiesLabel,
 //           backgroundColor: "blue",
 //           borderColor: "rgba(255, 99, 132, 1)",
-          
+
 //         },
 //         {
 //           label: "Género",
 //           data: generoLabel,
 //           backgroundColor: "pink",
 //           borderColor: "rgba(255, 99, 132, 1)",
-          
+
 //         },
 //       ],
 //     },
@@ -49,22 +48,20 @@ personajesChart();
 //       },
 //     },
 //   });
-  
 
 // COMIENZO CON LA API FETCH DE RICK Y MORTY
 async function getApiData() {
-    const url = "https://rickandmortyapi.com/api/character";
-    const response = await fetch(url);
-    const barChartData = await response.json();
-    //FUNCION MAP
-    const character = barChartData.results.map((x) => x.name);
-    const species = barChartData.results.map((x) => x.species);
-    const gender = barChartData.results.map((x) => x.gender);
-  
-    console.log(character, species);
-    personajesLabel = character;
+  const url = "http://localhost:3000/weekend";
+  const response = await fetch(url);
+  const barChartData = await response.json();
+  //FUNCION MAP
+  const character = barChartData.results.map((x) => x.name);
+  const species = barChartData.results.map((x) => x.species);
+  const gender = barChartData.results.map((x) => x.gender);
+
+  console.log(character, species);
+  personajesLabel = character;
   especiesLabel = species;
   generoLabel = gender;
 }
-console.log(personajesLabel)
-  
+console.log(personajesLabel);
