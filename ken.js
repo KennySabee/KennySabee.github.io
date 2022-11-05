dcf0c2be66757455545c19945f20436aea390caa
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2022-11-05 10:52:57 -0500

    fetÂat: avance chartÂ

--- ken.js
+++ ken.js
@@ -1,3 +1,33 @@
+
+//     type: "bar",
+//     data: {
+//       labels: personajesLabel,
+//       datasets: [
+//         {
+//           label: "Especie",
+//           data: especiesLabel,
+//           backgroundColor: "blue",
+//           borderColor: "rgba(255, 99, 132, 1)",
+          
+//         },
+//         {
+//           label: "Género",
+//           data: generoLabel,
+//           backgroundColor: "pink",
+//           borderColor: "rgba(255, 99, 132, 1)",
+          
+//         },
+//       ],
+//     },
+//     options: {
+//       tooltips: {
+//         mode: "index",
+//       },
+//     },
+//   });
+  
+
+// COMIENZO CON LA API FETCH DE RICK Y MORTY
 async function getApiData() {
     const url = "https://rickandmortyapi.com/api/character";
     const response = await fetch(url);
