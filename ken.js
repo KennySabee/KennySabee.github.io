45216018b7fd4b47eb55f0073d5923270a239207
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2022-11-05 19:44:48 -0500

    feat: nuevo ken

--- ken.js
+++ ken.js
@@ -1,13 +1,19 @@
+<<<<<<< HEAD
 let personajesLabel = [],
   especiesLabel = [],
   generoLabel = [];
+=======
+let diaLabel = [],  
+  horaLabel = [];
+>>>>>>> 6f2ab22 (feat: api nueva dbjson grafico en blanco)
 
 // GRAFICO DE CHART JS
-async function personajesChart() {
+async function horaEntradaChart() {
   await getApiData();
 
-  const grafico = document.getElementById("myChart").getContext("2d");
+  const grafico = document.getElementById('myChart').getContext("2d");
   const myChart = new Chart(grafico, {
+<<<<<<< HEAD
     type: "pie",
     data: {
       datasets: [
@@ -48,9 +54,41 @@ personajesChart();
 //       },
 //     },
 //   });
+=======
+    type: "bar",
+        data: {
+          labels: diaLabel,
+          datasets: [
+            {
+              label: "Dia",
+              data: diaLabel,
+              backgroundColor: "blue",
+              borderColor: "rgba(255, 99, 132, 1)",
+    
+            },
+            {
+              label: "Hora entrada",
+              data: horaLabel,
+              backgroundColor: "pink",
+              borderColor: "rgba(255, 99, 132, 1)",
+    
+            },
+          ],
+        },
+        options: {
+          tooltips: {
+            mode: "index",
+          },
+        },
+      });
+      }
 
-// COMIENZO CON LA API FETCH DE RICK Y MORTY
+horaEntradaChart();
+>>>>>>> 6f2ab22 (feat: api nueva dbjson grafico en blanco)
+
+// se cambio a la api de dbjson NO SALE LOS GRAFICOS GRAFIA EN BLANCO 
 async function getApiData() {
+<<<<<<< HEAD
   const url = "http://localhost:3000/weekend";
   const response = await fetch(url);
   const barChartData = await response.json();
@@ -65,3 +103,20 @@ async function getApiData() {
   generoLabel = gender;
 }
 console.log(personajesLabel);
+=======
+    const url = "http://localhost:3000/Hora_entrada";
+    const response = await fetch(url);
+    const barChartData = await response.json();
+    //FUNCION MAP
+   
+    const horaEntrada = barChartData.map((x) => x.value);
+    const dia = barChartData.map((x) => x.description);
+      
+    console.log(horaEntrada, dia);
+    horaLabel = horaEntrada;
+    diaLabel = dia;
+    
+  }
+  console.log(diaLabel)
+
+>>>>>>> 6f2ab22 (feat: api nueva dbjson grafico en blanco)
---
+++ ken2.0/ken2.0.js
@@ -0,0 +1,55 @@
+let diaLabel = [],  
+  horaLabel = [];
+
+// GRAFICO DE CHART JS
+async function horaEntradaChart() {
+  await getApiData();
+
+  const grafico = document.getElementById('myChart').getContext("2d");
+  const myChart = new Chart(grafico, {
+    type: "bar",
+        data: {
+          labels: diaLabel,
+          datasets: [
+            {
+              label: "Dia",
+              data: diaLabel,
+              backgroundColor: "blue",
+              borderColor: "rgba(255, 99, 132, 1)",
+    
+            },
+            {
+              label: "Hora entrada",
+              data: horaLabel,
+              backgroundColor: "pink",
+              borderColor: "rgba(255, 99, 132, 1)",
+    
+            },
+          ],
+        },
+        options: {
+          tooltips: {
+            mode: "index",
+          },
+        },
+      });
+      }
+
+horaEntradaChart();
+
+async function getApiData() {
+    const url = "http://localhost:3000/Hora_entrada";
+    const response = await fetch(url);
+    const barChartData = await response.json();
+    //FUNCION MAP
+   
+    const horaEntrada = barChartData.map((x) => x.value);
+    const dia = barChartData.map((x) => x.description);
+      
+    console.log(horaEntrada, dia);
+    horaLabel = horaEntrada;
+    diaLabel = dia;
+    
+  }
+  console.log(diaLabel)
+
